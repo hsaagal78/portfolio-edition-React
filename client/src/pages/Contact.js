72347488861ef6fa1e9313b0951795a42c581884
hsaagal78 <hsagal78@gmail.com>
2023-08-06 15:46:50 -0400

    update portfolio and sass, also create contact

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,84 @@
+import { useState } from 'react';
+
 function Contact() {
-    return (
-      <main>
-        <h1>Contact</h1>
-      </main>
-    )
+  const [formData, setFormData] = useState({
+    userName: '',
+    email: '',
+    message: ''
+  });
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const handleInputChange = event => {
+    const stateProp = event.target.name;
+    const value = event.target.value;
+
+    setFormData({
+      ...formData,
+      [stateProp]: value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const url = 'https://api.web3forms.com/submit';
+    const access_key = '6f414466-9832-4b96-b209-4952a1eefff5';
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ...formData,
+        access_key
+      })
+    }).then(res => res.json())
+      .then(data => {
+        setFormData({
+          userName: '',
+          email: '',
+          message: ''
+        });
+        setShowConfirmation(true);
+
+        setTimeout(() => setShowConfirmation(false), 3500);
+      })
+      .catch(err => console.log(err));
   }
-  
-  export default Contact;
+
+  return (
+    <main>
+      <h1 className="text-center">Contact Me</h1>
+
+      {showConfirmation && <p className="text-center confirmation">Thanks for your message! I'll get back to you soon.</p>}
+
+      <form
+        onSubmit={handleSubmit}
+        className="column">
+        <input
+          name="userName"
+          onChange={handleInputChange}
+          value={formData.userName}
+          type="text"
+          placeholder="Enter your name" />
+        <input
+          name="email"
+          onChange={handleInputChange}
+          value={formData.email}
+          type="email"
+          placeholder="Enter your email address" />
+        <textarea
+          name="message"
+          onChange={handleInputChange}
+          value={formData.message}
+          cols="30"
+          rows="10"
+          placeholder="Enter your message"></textarea>
+        <button>Submit</button>
+      </form>
+    </main>
+  )
+}
+
+export default Contact;
--- client/src/pages/Porfolio.js
+++ client/src/pages/Porfolio.js
@@ -3,9 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons'; 
 
 import noteTaker from '../asses/noteTaker.png'
-// import rype from '../asses/rype.png'
-// import techBlog from '../asses/techBlog.png'
-// import textEditor from '../asses/textEditor.png'
+import rype from '../asses/rype.png'
+import techBlog from '../asses/techBlog.png'
+import textEditor from '../asses/textEditor.png'
+import codingQuiz from '../asses/codingQuiz.png'
+import wheatherP from '../asses/wheatherP.png'
+
 
 
 function Portfolio() {
@@ -14,9 +17,11 @@ function Portfolio() {
       <section className='porfolio-noteTaker'>
         
         <div className='link-noteTaker'>
+        <h1>Note Taker</h1>
+         <h2>App that allwos user to create,update and delete notes</h2>
         <a 
         className='link-noteTaker'
-        href="https://fathomless-coast-62859-4d8464296632.herokuapp.com" 
+        href="https://lit-caverns-20017-2064580b596c.herokuapp.com" 
         target="_blank" 
         rel="noopener noreferrer"
         ><img src={noteTaker} 
@@ -24,12 +29,92 @@ function Portfolio() {
          alt= 'noteTaker' /></a
         >
         <a className='github-logo' href="https://github.com/hsaagal78/noteTakerCode"><FontAwesomeIcon icon={faGithub} /></a>
-        <h1>Note Taker</h1>
-         <h2>App that allwos user to create,update and delete notes</h2>
+       
+         
+       </div>
+       <div className='link-noteTaker'>
+        <h1>Text Editor</h1>
+         <h2> Create,manage notes or code, regardless connectivity</h2>
+        <a 
+        className='link-noteTaker'
+        href="https:fathomless-coast-62859-4d8464296632.herokuapp.com" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        ><img src={textEditor} 
+        className='img-noteTaker'
+         alt= 'textEditor' /></a
+        >
+        <a className='github-logo' href="https://github.com/hsaagal78/PWA-TextEditor"><FontAwesomeIcon icon={faGithub} /></a>
+       
+         
+       </div>
+       <div className='link-noteTaker'>
+        <h1>Tech Blog</h1>
+         <h2> Blogging to share their thoughts through blog posts</h2>
+        <a 
+        className='link-noteTaker'
+        href="https://fierce-fortress-39623-eda80897ed13.herokuapp.com" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        ><img src={techBlog} 
+        className='img-noteTaker'
+         alt= 'noteTaker' /></a
+        >
+        <a className='github-logo' href="https://github.com/hsaagal78/BlogMVCCentral"><FontAwesomeIcon icon={faGithub} /></a>
+       
+         
+       </div>
+       <div className='link-noteTaker'>
+        <h1>Rype</h1>
+         <h2>Created appealing platform base ingredients have on hand</h2>
+        <a 
+        className='link-noteTaker'
+        href="https://vast-waters-05451-3dbd8c32d7c8.herokuapp.com/" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        ><img src={rype} 
+        className='img-noteTaker'
+         alt= 'noteTaker' /></a
+        >
+        <a className='github-logo' href="https://github.com/jeremytboyer/rype-project"><FontAwesomeIcon icon={faGithub} /></a>
+       
+         
+       </div>
+       <div className='link-noteTaker'>
+        <h1>Coding Quiz</h1>
+         <h2>App that allwos to take a Quiz.</h2>
+        <a 
+        className='link-noteTaker'
+        href="https://hsaagal78.github.io/ChallengeCodeQuiz/" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        ><img src={codingQuiz} 
+        className='img-noteTaker'
+         alt= 'noteTaker' /></a
+        >
+        <a className='github-logo' href="https://github.com/hsaagal78/ChallengeCodeQuiz"><FontAwesomeIcon icon={faGithub} /></a>
+       
+         
+       </div>
+       <div className='link-noteTaker'>
+        <h1>Wheater Dashboard</h1>
+         <h2>The forecast for the next five days for a specific city</h2>
+        <a 
+        className='link-noteTaker'
+        href="https://hsaagal78.github.io/Dashboard_Weather_challenge/" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        ><img src={wheatherP} 
+        className='img-noteTaker'
+         alt= 'noteTaker' /></a
+        >
+        <a className='github-logo' href="https://github.com/hsaagal78/Dashboard_Weather_challenge"><FontAwesomeIcon icon={faGithub} /></a>
+       
          
        </div>
         
       </section>
+      
     )
   }
   
