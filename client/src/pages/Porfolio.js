2354db5a1072a63bfb6d74772c45e18d87a42454
hsaagal78 <hsagal78@gmail.com>
2023-08-05 21:49:25 -0400

    new pictures for portfolio and update portfolio file

--- client/src/pages/Porfolio.js
+++ client/src/pages/Porfolio.js
@@ -1,9 +1,31 @@
+import React from 'react';
+import noteTaker from '../asses/noteTaker.png'
+import rype from '../asses/rype.png'
+import techBlog from '../asses/techBlog.png'
+import textEditor from '../asses/textEditor.png'
 function Portfolio() {
     return (
-      <main>
+     
+      <ul className='porfolio-contaner'>
         <h1>Portfolio</h1>
-      </main>
+        <li>
+        <a href='https:fathomless-coast-62859-4d8464296632.herokuapp.com'></a>
+        <img src={noteTaker} 
+        className='img-noteTaker'
+         alt= 'noteTaker' />
+         <h1>Note Taker</h1>
+         <h2>App that allos user to create,update and delete notes</h2>
+        </li>
+
+        
+        
+        </ul>
     )
   }
   
   export default Portfolio;
+
+  // https:fathomless-coast-62859-4d8464296632.herokuapp.com/(text editor)
+ 
+        // https://fierce-fortress-39623-eda80897ed13.herokuapp.com/(Tech Blog)
+        // https://lit-caverns-20017-2064580b596c.herokuapp.com/(Note Taker)
